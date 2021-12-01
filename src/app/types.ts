3a4a31eb185c926f0c4d4d4af929cb930b97931d
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-12-01 17:27:45 +0200

    exporting of type defs updated

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,7 +1,7 @@
-type CardSuit = "clubs" | "spades" | "hearts" | "diamonds";
-type CardNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
+export type CardSuit = "clubs" | "spades" | "hearts" | "diamonds";
+export type CardNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
 
-type Card = {
+export type Card = {
   number: CardNumber;
   suit: CardSuit;
 };
