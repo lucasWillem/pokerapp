623365e5dc883d8ee481d824fb4ede6e46969bdc
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-12-01 20:15:07 +0200

    straight flush evaluator completed

--- src/app/hand-evaluators/evaluators/StraighFlushEvaluator.ts
+++ src/app/hand-evaluators/evaluators/StraighFlushEvaluator.ts
@@ -6,7 +6,43 @@ export default class StraightFlushEvaluator implements HandEvaluator {
 
   public evaluate() {
     const { hand } = this;
-    console.log("evaluate this hand");
+    const handOfRelevantCardsInAscendingOrder = hand
+      // .filter((card) => {
+      //   return (
+      //     card.number !== 1 ||
+      //     card.number !== 10 ||
+      //     card.number !== 11 ||
+      //     card.number !== 12 ||
+      //     (card.number !== 13 && card)
+      //   );
+      // })
+      .sort((a, b) => a.number - b.number);
+
+    console.log(handOfRelevantCardsInAscendingOrder);
+
+    let firstPointer = 0;
+    let secondPointer = 1;
+
+    while (secondPointer <= handOfRelevantCardsInAscendingOrder.length - 1) {
+      const firstCard = handOfRelevantCardsInAscendingOrder[firstPointer];
+      const secondCard = handOfRelevantCardsInAscendingOrder[secondPointer];
+
+      if (firstCard.number === 1) {
+        return false;
+      }
+
+      if (secondCard.number !== firstCard.number + 1) {
+        return false;
+      }
+
+      if (firstCard.suit !== secondCard.suit) {
+        return false;
+      }
+
+      firstPointer++;
+      secondPointer++;
+    }
+
     return true;
   }
 }
