51cb11439af9ea76b2184f90ebc3e597cedd0761
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-12-02 01:54:40 +0200

    pair evaluator strategy changed to make use of frequency counter pattern rather than multiple pointer pattern

--- src/app/hand-evaluators/evaluators/PairEvaluator.ts
+++ src/app/hand-evaluators/evaluators/PairEvaluator.ts
@@ -1,29 +1,31 @@
 import type { HandEvaluator } from "../types";
 import type { Hand } from "../../types";
 
+interface Frequencies {
+  [key: string]: any;
+}
+
 export default class PairEvaluator implements HandEvaluator {
   constructor(private hand: Hand) {}
 
   public evaluate() {
     const { hand } = this;
-    const handSortedAscending = hand.sort((a, b) => a.number - b.number);
+    const frequencies: Frequencies = {};
+    let isPair = false;
 
-    let numOfmatches = 0;
-
-    let firstPointer = 0;
-    let secondPointer = 1;
-
-    while (secondPointer <= handSortedAscending.length - 1) {
-      const firstCard = handSortedAscending[firstPointer];
-      const secondCard = handSortedAscending[secondPointer];
-
-      if (firstCard.number === secondCard.number) {
-        numOfmatches += 1;
-      }
-      firstPointer++;
-      secondPointer++;
+    for (const card of hand) {
+      const cardProperty = card.number;
+      frequencies[cardProperty] = ++frequencies[cardProperty] || 1;
     }
 
-    return numOfmatches === 1 ? true : false;
+    for (const [cardNumber, cardNumberFrequency] of Object.entries(
+      frequencies
+    )) {
+      if (cardNumberFrequency === 2) {
+        isPair = true;
+      }
+    }
+
+    return isPair;
   }
 }
