d8884eb8573efb611f1d8136f37b5d23f03f2eaa
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-12-01 17:51:15 +0200

    changed GameStarter to generate hands

--- src/app/play/GamerStarter.ts
+++ src/app/play/GamerStarter.ts
@@ -1,16 +1,20 @@
 import { DeckCreatorContext, DefaultDeckCreator } from "../deck";
 import { ShufflerContext, RandomShuffler } from "../shufflers";
+import { DealerContext, RandomDealer } from "../deck-dealers";
 
 export default abstract class GameStarter {
-  static start() {
+  static getHands({ noOfPlayers = 1 }: { noOfPlayers?: number }) {
+    const hands = [];
+
     const deck = new DeckCreatorContext(new DefaultDeckCreator()).create();
     const shuffledDeck = new ShufflerContext(
       new RandomShuffler(deck)
     ).shuffle();
 
-    console.log(shuffledDeck);
+    for (let x = 1; x <= noOfPlayers; x++) {
+      hands.push(new DealerContext(new RandomDealer(shuffledDeck)).deal());
+    }
 
-    // shuffle the deck
-    // deal cards
+    return hands;
   }
 }
