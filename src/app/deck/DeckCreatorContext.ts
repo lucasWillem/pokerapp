48322f5370f283e58165fa89a5fb7b10562e1380
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-12-01 17:26:28 +0200

    deck creators added

---
+++ src/app/deck/DeckCreatorContext.ts
@@ -0,0 +1,9 @@
+import type { CreatorContext, DeckCreator } from "./types";
+
+export default class DeckCreatorContext implements CreatorContext {
+  constructor(private deckCreator: DeckCreator) {}
+
+  create() {
+    return this.deckCreator.create();
+  }
+}
