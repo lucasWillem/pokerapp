d249fd485dc93bc36ae4207754520e93aeb3b5a3
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-12-01 21:29:28 +0200

    flush evaluator strategy completed

--- src/app/hand-evaluators/index.ts
+++ src/app/hand-evaluators/index.ts
@@ -2,10 +2,12 @@ import HandEvalutorsContext from "./HandEvalutorsContext";
 import RoyalFlushEvaluator from "./evaluators/RoyalFlushEvaluator";
 import StraighFlushEvaluator from "./evaluators/StraighFlushEvaluator";
 import FourOfAKindEvaluator from "./evaluators/FourOfAKindEvaluator";
+import FlushEvaluator from "./evaluators/FlushEvaluator";
 
 export {
   HandEvalutorsContext,
   RoyalFlushEvaluator,
   StraighFlushEvaluator,
   FourOfAKindEvaluator,
+  FlushEvaluator,
 };
