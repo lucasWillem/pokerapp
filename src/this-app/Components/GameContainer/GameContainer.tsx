8c8c5b47e41ffa25ce6530543a16f572ea74ae59
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-12-02 22:46:54 +0200

    Player container component instantiated in gameContainer
    GameContainer gave width and height

--- src/this-app/Components/GameContainer/GameContainer.tsx
+++ src/this-app/Components/GameContainer/GameContainer.tsx
@@ -1,9 +1,13 @@
 import React, { memo } from "react";
 import ActionButton from "../../../global/Components/ActionButton";
+import PlayerContainer from "../PlayerContainer";
+
+import "./GameContainer.css";
 
 const GameContainer = function () {
   return (
-    <div>
+    <div className="GameContainer">
+      <PlayerContainer />
       <ActionButton buttonText="Start game" />
     </div>
   );
