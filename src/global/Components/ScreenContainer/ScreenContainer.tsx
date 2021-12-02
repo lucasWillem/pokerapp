a2ee9faa6530f6680e28100d84a4bcf84f4f8c77
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-12-02 13:11:50 +0200

    some css added to ScreenContainer to give it margin

--- src/global/Components/ScreenContainer/ScreenContainer.tsx
+++ src/global/Components/ScreenContainer/ScreenContainer.tsx
@@ -1,13 +1,17 @@
 import React, { memo, ReactNode } from "react";
 import { Container } from "react-bootstrap";
+import "./ScreenContainer.css";
 
 type Props = {
   children: ReactNode;
-  isColor?: "blue";
 };
 
 const ScreenContainer = function ({ children }: Props) {
-  return <Container>{children}</Container>;
+  return (
+    <Container className="ScreenContainer" fluid>
+      {children}
+    </Container>
+  );
 };
 
 export default memo(ScreenContainer);
