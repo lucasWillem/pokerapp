be275981c885126c1254cc4e2eb374c2dbdb8f3f
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-12-03 03:47:06 +0200

    Dropdown component added and implemented to select number of players

--- src/app/Components/GameContainer/GameContainer.tsx
+++ src/app/Components/GameContainer/GameContainer.tsx
@@ -1,13 +1,17 @@
-import React, { memo, useCallback } from "react";
+import React, { memo, useCallback, useState, useEffect } from "react";
+
+import { useStoreActions, useStoreState } from "../../../redux";
 
 import ActionButton from "../../../global/Components/ActionButton";
 import PlayerContainer from "../PlayerContainer";
 import GameStarter from "../../../global/play";
-import { useStoreActions, useStoreState } from "../../../redux";
+import DropDown from "../../../global/Components/DropDown";
 
 import "./GameContainer.css";
 
 const GameContainer = function () {
+  const [selection, setSelection] = useState(0);
+
   const storeHands = useStoreActions(
     (actions) => actions.playersHands.storePokerHands
   );
@@ -15,15 +19,40 @@ const GameContainer = function () {
   const pokerHands = useStoreState((state) => state.playersHands.pokerHands);
 
   const startGame = useCallback(() => {
-    const hands = GameStarter.getHands({ noOfPlayers: 10 });
+    const hands = GameStarter.getHands({ noOfPlayers: selection });
     storeHands(hands);
-  }, [storeHands]);
+  }, [selection, storeHands]);
+
+  const storeNumberOfPlayers = useCallback((menuItem) => {
+    setSelection(menuItem);
+  }, []);
 
   return (
     <div className="GameContainer">
       <PlayerContainer pokerHands={pokerHands} />
       {pokerHands.length === 0 && (
-        <ActionButton buttonText="Start game" action={startGame} />
+        <div
+          style={{
+            maxHeight: 200,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <div style={{ marginRight: 20 }}>
+            <DropDown
+              title="Select Number Of Players"
+              menuItems={[2, 3, 4, 5, 6]}
+              makeSelection={storeNumberOfPlayers}
+              activeItem={selection}
+            />
+          </div>
+          <ActionButton
+            disabled={selection === 0 ? true : false}
+            buttonText="Start game"
+            action={startGame}
+          />
+        </div>
       )}
     </div>
   );
