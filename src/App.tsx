94595f4dac9b6f1944c8802d51edf7fc50804a7e
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-06-09 19:07:14 +0200

    task:  remove Alert from App.tsx, move into PokerGame container in order to use controlled component design pattern

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import Navigator from "./navigation/Navigator";
-import { Alert } from "./components/containers/Alert";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Alert />
         <Navigator />
       </BrowserRouter>
     </div>
