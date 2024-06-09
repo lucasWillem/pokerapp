261095e7b37329e3b80fb1217e9fbd5d2e3c3fae
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-06-09 19:28:46 +0200

    feature:  replace wrappers/ScreenContainer with template/ScreenTemplate

---
+++ src/components/templates/ScreenTemplate/ScreenTemplate.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Container } from "react-bootstrap";
+
+const StyledScreenTemplate = styled(Container)`
+  padding: 5%;
+  background: ${(props) => props.theme.colors.dark};
+`;
+
+export { StyledScreenTemplate };
---
+++ src/components/templates/ScreenTemplate/ScreenTemplate.template.tsx
@@ -0,0 +1,16 @@
+import React, { memo, ReactNode, FC } from "react";
+import { StyledScreenTemplate } from "./ScreenTemplate.styles";
+
+interface ScreenWrapperProps {
+  children: ReactNode;
+}
+
+const ScreenTemplate: FC<ScreenWrapperProps> = ({ children }) => {
+  return (
+    <StyledScreenTemplate fluid>
+      {children}
+    </StyledScreenTemplate>
+  );
+};
+
+export default memo(ScreenTemplate);
