737c0cd65b2c5b5fad3209402ab3bb8de330cbb6
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-06-11 12:39:51 +0200

    task: configure import aliases, in eslint, webpack and tsConfig.

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,6 +1,7 @@
 import { memo, FC } from 'react';
-import { ScreenTemplate } from '../components/templates/ScreenTemplate';
-import { PokerGame } from '../components/containers/PokerGame';
+
+import { ScreenTemplate } from '@components/templates/ScreenTemplate';
+import { PokerGame } from '@components/containers/PokerGame';
 
 const GamePage: FC = () => {
   return (
