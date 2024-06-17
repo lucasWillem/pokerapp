bbfd1c36342aaf62a6edff7ff0754ce45d4770c7
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-06-17 03:07:08 +0200

    feature: port app over to vite and add error boundary

---
+++ src/components/templates/ErrorBoundary/ErrorBoundary.styles.ts
@@ -0,0 +1,47 @@
+import { ColorOptions } from "@global/theme";
+import { Alert } from "react-bootstrap";
+import styled, { css } from "styled-components";
+
+const StyledErrorBoundaryWrapper = styled("div")(({ theme }) =>
+  css({
+    background: theme.colors.black,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    width: "100%",
+  }),
+);
+
+const StyledErrorFallback = styled(Alert)(({ theme }) =>
+  css({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "20vh auto",
+    width: "50%",
+    backgroundColor: theme.colors[ColorOptions.Black],
+    border: `1px solid ${theme.colors[ColorOptions.Red]}`,
+  }),
+);
+
+const StyledAlertHeading = styled(Alert.Heading)(({ theme }) =>
+  css({
+    color: theme.colors.light,
+  }),
+);
+
+const StyledMessage = styled("p")(({ theme }) =>
+  css({
+    color: theme.colors.light,
+  }),
+);
+
+export {
+  StyledErrorBoundaryWrapper,
+  StyledErrorFallback,
+  StyledAlertHeading,
+  StyledMessage,
+};
