900f6dec3c7252ecb6174e401f872b9c6a5d84e2
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-06-18 23:36:20 +0200

    feature:  add winning hand to congratulations message

--- src/components/containers/PokerGame/features/game/types.ts
+++ src/components/containers/PokerGame/features/game/types.ts
@@ -1,1 +1,14 @@
-export type PlayerRank = { player: number; rank: number };
+export const enum WinningHand {
+  RoyalFlush = "Royal Flush",
+  StraightFlush = "Straight Flush",
+  FourOfAKind = "Four of a Kind",
+  FullHouse = "Full House",
+  Flush = "Flush",
+  Straight = "Straight",
+  ThreeOfAKind = "Three of a Kind",
+  TwoPair = "Two Pair",
+  Pair = "Pair",
+  HighCard = "High Card",
+}
+
+export type PlayerRank = { player: number; rank: number; name: WinningHand };
